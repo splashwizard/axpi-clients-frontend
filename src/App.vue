6e73601ab293f0ff019ab628f4f16cc1d815e1d0
Harvey Coplestone <harvey@enhanceable.io>
2021-02-10 12:24:56 +0000

    login form

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,118 @@
 <template>
     <div id="app">
-        <a-layout id="components-layout-demo-responsive">
-            <a-layout-sider
-                    width="275"
-                    breakpoint="lg"
-                    theme="dark"
-                    collapsed-width="0"
-                    @collapse="onCollapse"
-                    @breakpoint="onBreakpoint"
-                    :style="{ background: '#f7fafc', borderRight: '1px solid #e3e8ee' }"
-            >
-                <div class="logo">
-                    <img src="/img/axiom.png" alt="">
-                </div>
-                <a-menu
-                        :default-selected-keys="['1']"
-                        :default-open-keys="['sub1']"
-                        mode="inline"
-                >
-                    <a-menu-item key="1">
-                        <a-icon type="mail"/>
-                        Navigation One
-                    </a-menu-item>
-                    <a-menu-item key="2">
-                        <a-icon type="calendar"/>
-                        Navigation Two
-                    </a-menu-item>
-                    <a-sub-menu key="sub1">
-                        <span slot="title"><a-icon type="appstore"/><span>Navigation Three</span></span>
-                        <a-menu-item key="3">
-                            Option 3
-                        </a-menu-item>
-                        <a-menu-item key="4">
-                            Option 4
-                        </a-menu-item>
-                        <a-sub-menu key="sub1-2" title="Submenu">
-                            <a-menu-item key="5">
-                                Option 5
-                            </a-menu-item>
-                            <a-menu-item key="6">
-                                Option 6
-                            </a-menu-item>
-                        </a-sub-menu>
-                    </a-sub-menu>
-                    <a-sub-menu key="sub2">
-                        <span slot="title"><a-icon type="setting"/><span>Navigation Four</span></span>
-                        <a-menu-item key="7">
-                            Option 7
-                        </a-menu-item>
-                        <a-menu-item key="8">
-                            Option 8
-                        </a-menu-item>
-                        <a-menu-item key="9">
-                            Option 9
-                        </a-menu-item>
-                        <a-menu-item key="10">
-                            Option 10
-                        </a-menu-item>
-                    </a-sub-menu>
-                </a-menu>
-            </a-layout-sider>
-            <a-layout>
-                <a-layout-header :style="{ background: '#fff', padding: '0 30px', borderBottom: '1px solid #e3e8ee' }">
-                    <div class="top-nav">
-                        <div class="top-nav-search-bar">
-                            <a-input ref="userNameInput" placeholder="Search..." size="large">
-                                <a-icon slot="prefix" type="search"/>
-                            </a-input>
-                        </div>
-                        <div class="top-nav-links">
-                            <div id="nav">
-                                <a-button icon="message">Feedback</a-button>
-                                <a-button icon="bell"></a-button>
-                                <a-button icon="question"></a-button>
-                                <a-button icon="user"></a-button>
-                            </div>
-                        </div>
-                    </div>
-                </a-layout-header>
-                <a-layout-content :style="{ padding: '30px', background: '#fff' }">
-                    <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
-                        <router-view/>
-                    </div>
-                </a-layout-content>
-            </a-layout>
-        </a-layout>
+<!--        <a-layout id="components-layout-demo-responsive">-->
+<!--            <a-layout-sider-->
+<!--                    width="250"-->
+<!--                    breakpoint="lg"-->
+<!--                    theme="dark"-->
+<!--                    collapsed-width="0"-->
+<!--                    @collapse="onCollapse"-->
+<!--                    @breakpoint="onBreakpoint"-->
+<!--                    :style="{ background: '#14144f', borderRight: '1px solid #e3e8ee' }"-->
+<!--            >-->
+<!--                <div class="logo">-->
+<!--                    <img src="/img/axiom-dark-bg.png" alt="">-->
+<!--                </div>-->
+<!--                <a-menu-->
+<!--                        :default-selected-keys="['1']"-->
+<!--                        :default-open-keys="['sub1']"-->
+<!--                        mode="inline"-->
+<!--                        theme="dark"-->
+<!--                >-->
+<!--                    <a-menu-item key="home">-->
+<!--                        <a-icon type="home"/>-->
+<!--                        Home-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="orders">-->
+<!--                        <a-icon type="shopping-cart"/>-->
+<!--                        Orders-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="suppliers">-->
+<!--                        <a-icon type="bars"/>-->
+<!--                        Suppliers-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="goals">-->
+<!--                        <a-icon type="rise"/>-->
+<!--                        Goals-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="analytics">-->
+<!--                        <a-icon type="dashboard"/>-->
+<!--                        Optimisation-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="analytics">-->
+<!--                        <a-icon type="eye"/>-->
+<!--                        Analytics-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="reports">-->
+<!--                        <a-icon type="bar-chart"/>-->
+<!--                        Reports-->
+<!--                    </a-menu-item>-->
+<!--&lt;!&ndash;                    <a-sub-menu key="sub1">&ndash;&gt;-->
+<!--&lt;!&ndash;                        <span slot="title"><a-icon type="appstore"/><span>Navigation Three</span></span>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="3">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 3&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="4">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 4&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-sub-menu key="sub1-2" title="Submenu">&ndash;&gt;-->
+<!--&lt;!&ndash;                            <a-menu-item key="5">&ndash;&gt;-->
+<!--&lt;!&ndash;                                Option 5&ndash;&gt;-->
+<!--&lt;!&ndash;                            </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                            <a-menu-item key="6">&ndash;&gt;-->
+<!--&lt;!&ndash;                                Option 6&ndash;&gt;-->
+<!--&lt;!&ndash;                            </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-sub-menu>&ndash;&gt;-->
+<!--&lt;!&ndash;                    </a-sub-menu>&ndash;&gt;-->
+<!--&lt;!&ndash;                    <a-sub-menu key="sub2">&ndash;&gt;-->
+<!--&lt;!&ndash;                        <span slot="title"><a-icon type="chart"/><span>Reports</span></span>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="7">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 7&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="8">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 8&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="9">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 9&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                        <a-menu-item key="10">&ndash;&gt;-->
+<!--&lt;!&ndash;                            Option 10&ndash;&gt;-->
+<!--&lt;!&ndash;                        </a-menu-item>&ndash;&gt;-->
+<!--&lt;!&ndash;                    </a-sub-menu>&ndash;&gt;-->
+<!--                    <a-menu-item key="developer">-->
+<!--                        <a-icon type="code"/>-->
+<!--                        Developers-->
+<!--                    </a-menu-item>-->
+<!--                    <a-menu-item key="account">-->
+<!--                        <a-icon type="user"/>-->
+<!--                        Account-->
+<!--                    </a-menu-item>-->
+<!--                </a-menu>-->
+<!--            </a-layout-sider>-->
+<!--            <a-layout>-->
+<!--                <a-layout-header :style="{ background: '#fff', padding: '0 30px', borderBottom: '1px solid #e3e8ee' }">-->
+<!--                    <div class="top-nav">-->
+<!--                        <div class="top-nav-search-bar">-->
+<!--                            <a-input ref="userNameInput" placeholder="Search..." size="large">-->
+<!--                                <a-icon slot="prefix" type="search"/>-->
+<!--                            </a-input>-->
+<!--                        </div>-->
+<!--                        <div class="top-nav-links">-->
+<!--                            <div id="nav">-->
+<!--                                <a-button icon="message">Feedback</a-button>-->
+<!--                                <a-button icon="bell"></a-button>-->
+<!--                                <a-button icon="question"></a-button>-->
+<!--                                <a-button icon="user"></a-button>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </a-layout-header>-->
+<!--                <a-layout-content :style="{ padding: '30px', background: '#fff' }">-->
+<!--                    <router-view/>-->
+<!--                </a-layout-content>-->
+<!--            </a-layout>-->
+<!--        </a-layout>-->
+        <router-view/>
     </div>
 </template>
 <script>
@@ -104,27 +132,32 @@
         height: 100%;
     }
 
-    .top-nav {
-        display: flex;
+    .ant-layout-header {
+        height: 60px !important;
+        line-height: 60px !important;
 
-        .top-nav-search-bar {
-            flex-grow: 1;
+        .top-nav {
+            display: flex;
 
-            input {
-                border: 0;
-                font-size: 15px;
+            .top-nav-search-bar {
+                flex-grow: 1;
 
-                &:hover, &:focus {
-                    box-shadow: none;
+                input {
+                    border: 0;
+                    font-size: 15px;
+
+                    &:hover, &:focus {
+                        box-shadow: none;
+                    }
                 }
             }
-        }
 
-        .top-nav-links {
-            flex-shrink: 1;
+            .top-nav-links {
+                flex-shrink: 1;
 
-            .ant-btn {
-                margin-left: 5px;
+                .ant-btn {
+                    margin-left: 5px;
+                }
             }
         }
     }
@@ -133,17 +166,23 @@
         .logo {
             text-align: left;
             padding-left: 20px;
-            padding-top: 20px;
-            padding-bottom: 20px;
+            padding-top: 30px;
+            padding-bottom: 30px;
         }
 
         .logo img {
-            max-width: 130px;
+            max-width: 140px;
         }
 
         .ant-menu, .ant-menu-submenu {
             border: 0;
             background: none !important;
+        }
+
+        .ant-menu-item.ant-menu-item-selected {
+            background-color: unset !important;
+            font-weight: 600;
+            color: #fff;
         }
     }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="login-page">
+        <div class="login-page-inner">
+            <img src="/img/axiom-icon.png" alt="Logo" width="70">
+
+            <div class="login-form-card">
+                <h1>Welcome Back!</h1>
+                <svg class="block mx-auto mb-6" xmlns="http://www.w3.org/2000/svg" width="100" height="2"
+                     viewBox="0 0 100 2">
+                    <path fill="#D8E3EC" d="M0 0h100v2H0z"></path>
+                </svg>
+
+                <a-form
+                        :form="form"
+                        class="login-form"
+                        @submit="handleSubmit"
+                >
+                    <a-form-item>
+                        <a-input
+                                size="large"
+                                v-decorator="[
+          'email',
+          { rules: [{ required: true, message: 'Please input your email' }, {type: 'email', message: 'Please enter a valid email address'}] },
+        ]"
+                                placeholder="Email"
+                        >
+                            <a-icon slot="prefix" type="mail" style="color: rgba(0,0,0,.25)"/>
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-input
+                                size="large"
+                                v-decorator="[
+          'password',
+          { rules: [{ required: true, message: 'Please input your password' }] },
+        ]"
+                                type="password"
+                                placeholder="Password"
+                        >
+                            <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)"/>
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" size="large" html-type="submit" class="login-form-button">
+                            Log In
+                        </a-button>
+                        <a href="">
+                            Forgot password
+                        </a>
+                    </a-form-item>
+                </a-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {
+                form: this.$form.createForm(this, { name: 'login_form' })
+            }
+        },
+        methods: {
+            handleSubmit(e) {
+                e.preventDefault();
+                this.form.validateFields((err, values) => {
+                    if (!err) {
+                        this.login(values);
+                    }
+                });
+            },
+
+            login(values) {
+                console.log(values);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .login-page {
+        background: #eef1f4;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .login-form-card {
+            margin-top: 40px;
+            box-shadow: 0 2px 4px 0 rgb(0 0 0 / 5%);
+            max-width: 25rem;
+            width: 25rem;
+            padding: 2rem;
+            background: #fff;
+            border-radius: .5rem;
+
+            .login-form {
+                margin-top: 30px;
+            }
+
+            .login-form-button {
+                width: 100%;
+                margin-bottom: 10px;
+            }
+        }
+    }
+</style>

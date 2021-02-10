866d9800174f0d08d237b87bbeceba9b3d7265d2
Harvey Coplestone <harvey@enhanceable.io>
2021-02-10 10:30:49 +0000

    axpi clinet frontend

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,149 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <a-layout id="components-layout-demo-responsive">
+            <a-layout-sider
+                    width="275"
+                    breakpoint="lg"
+                    theme="dark"
+                    collapsed-width="0"
+                    @collapse="onCollapse"
+                    @breakpoint="onBreakpoint"
+                    :style="{ background: '#f7fafc', borderRight: '1px solid #e3e8ee' }"
+            >
+                <div class="logo">
+                    <img src="/img/axiom.png" alt="">
+                </div>
+                <a-menu
+                        :default-selected-keys="['1']"
+                        :default-open-keys="['sub1']"
+                        mode="inline"
+                >
+                    <a-menu-item key="1">
+                        <a-icon type="mail"/>
+                        Navigation One
+                    </a-menu-item>
+                    <a-menu-item key="2">
+                        <a-icon type="calendar"/>
+                        Navigation Two
+                    </a-menu-item>
+                    <a-sub-menu key="sub1">
+                        <span slot="title"><a-icon type="appstore"/><span>Navigation Three</span></span>
+                        <a-menu-item key="3">
+                            Option 3
+                        </a-menu-item>
+                        <a-menu-item key="4">
+                            Option 4
+                        </a-menu-item>
+                        <a-sub-menu key="sub1-2" title="Submenu">
+                            <a-menu-item key="5">
+                                Option 5
+                            </a-menu-item>
+                            <a-menu-item key="6">
+                                Option 6
+                            </a-menu-item>
+                        </a-sub-menu>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub2">
+                        <span slot="title"><a-icon type="setting"/><span>Navigation Four</span></span>
+                        <a-menu-item key="7">
+                            Option 7
+                        </a-menu-item>
+                        <a-menu-item key="8">
+                            Option 8
+                        </a-menu-item>
+                        <a-menu-item key="9">
+                            Option 9
+                        </a-menu-item>
+                        <a-menu-item key="10">
+                            Option 10
+                        </a-menu-item>
+                    </a-sub-menu>
+                </a-menu>
+            </a-layout-sider>
+            <a-layout>
+                <a-layout-header :style="{ background: '#fff', padding: '0 30px', borderBottom: '1px solid #e3e8ee' }">
+                    <div class="top-nav">
+                        <div class="top-nav-search-bar">
+                            <a-input ref="userNameInput" placeholder="Search..." size="large">
+                                <a-icon slot="prefix" type="search"/>
+                            </a-input>
+                        </div>
+                        <div class="top-nav-links">
+                            <div id="nav">
+                                <a-button icon="message">Feedback</a-button>
+                                <a-button icon="bell"></a-button>
+                                <a-button icon="question"></a-button>
+                                <a-button icon="user"></a-button>
+                            </div>
+                        </div>
+                    </div>
+                </a-layout-header>
+                <a-layout-content :style="{ padding: '30px', background: '#fff' }">
+                    <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+                        <router-view/>
+                    </div>
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
     </div>
-    <router-view/>
-  </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    export default {
+        methods: {
+            onCollapse(collapsed, type) {
+                console.log(collapsed, type);
+            },
+            onBreakpoint(broken) {
+                console.log(broken);
+            },
+        }
     }
-  }
-}
+</script>
+<style lang="scss">
+    #app, .ant-layout {
+        height: 100%;
+    }
+
+    .top-nav {
+        display: flex;
+
+        .top-nav-search-bar {
+            flex-grow: 1;
+
+            input {
+                border: 0;
+                font-size: 15px;
+
+                &:hover, &:focus {
+                    box-shadow: none;
+                }
+            }
+        }
+
+        .top-nav-links {
+            flex-shrink: 1;
+
+            .ant-btn {
+                margin-left: 5px;
+            }
+        }
+    }
+
+    .ant-layout-sider {
+        .logo {
+            text-align: left;
+            padding-left: 20px;
+            padding-top: 20px;
+            padding-bottom: 20px;
+        }
+
+        .logo img {
+            max-width: 130px;
+        }
+
+        .ant-menu, .ant-menu-submenu {
+            border: 0;
+            background: none !important;
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// Ant Design
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
+
 new Vue({
   router,
   store,

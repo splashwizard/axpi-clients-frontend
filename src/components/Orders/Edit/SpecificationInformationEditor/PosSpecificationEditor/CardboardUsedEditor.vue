3d53878ca1340c7a0c8c5aa432dc85600d29632f
Harvey Coplestone <harvey@enhanceable.io>
2021-02-14 16:07:11 +0000

    cardboard used editor

--- src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/CardboardUsedEditor.vue
+++ src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/CardboardUsedEditor.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        asdf
+        <!-- EACH CARDBOARD USED REPEATER -->
+        <div v-for="(cardboard, i) in orderLocal.pos_cardboard_used" :key="i">
+            test
+        </div>
+        <!-- / EACH CARDBOARD USED REPEATER -->
     </div>
 </template>
 
 <script>
+    const CARDBOARD_USED_DATA_TEMPLATE = {
+        cardboard_type: null
+    };
     export default {
-        name: "CardboardUsedEditor"
+        name: "CardboardUsedEditor",
+        props: ['orderLocal'],
+        mounted() {
+            if (!this.orderLocal.pos_cardboard_used) {
+                this.orderLocal.pos_cardboard_used = [
+                    {
+                        ...CARDBOARD_USED_DATA_TEMPLATE
+                    }
+                ];
+            }
+        }
     }
 </script>
 

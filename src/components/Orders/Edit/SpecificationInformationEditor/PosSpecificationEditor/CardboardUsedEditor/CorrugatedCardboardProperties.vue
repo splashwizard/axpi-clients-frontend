289bcd968265ad719b471b90d9248f9c0f9bd720
Harvey Coplestone <harvey@enhanceable.io>
2021-02-14 20:18:50 +0000

    cardboard details

--- src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/CardboardUsedEditor/CorrugatedCardboardProperties.vue
+++ src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/CardboardUsedEditor/CorrugatedCardboardProperties.vue
@@ -1,24 +1,56 @@
 <template>
     <div>
         <div class="ant-col ant-form-item-label"><label title="Type of Cardboard" class="">Number Of Walls</label></div>
-        <number-of-walls-selector :cardboard="cardboard" @property-updated="handlePropertyUpdated"></number-of-walls-selector>
+        <number-of-walls-selector :cardboard="cardboard"
+                                  @property-updated="handlePropertyUpdated"></number-of-walls-selector>
+
+        <!-- Wall Details -->
+        <div class="wall-details" :key="updateKey">
+            <wall-details :cardboard="cardboard" v-for="wall in walls" :wall="wall" :key="wall"></wall-details>
+        </div>
+        <!-- / Wall Details -->
     </div>
 </template>
 
 <script>
     import NumberOfWallsSelector from "./CorrugatedCardboardProperties/NumberOfWallsSelector";
+    import WallDetails from "./CorrugatedCardboardProperties/WallDetails";
+
     export default {
         name: "CorrugatedCardboardProperties",
         props: ['cardboard'],
-        components: {NumberOfWallsSelector},
+        components: {NumberOfWallsSelector, WallDetails},
+        data() {
+            return {
+                updateKey: 1,
+                walls: []
+            }
+        },
         methods: {
             handlePropertyUpdated() {
                 this.$emit('property-updated');
+                this.determineWalls();
+                this.incrementUpdateKey();
+            },
+
+            incrementUpdateKey() {
+                this.updateKey = this.updateKey + 1;
+            },
+
+            determineWalls() {
+                let walls = [];
+                let i = 1;
+                for (i = 1; i <= this.cardboard.number_of_walls; i++) {
+                    walls.push(i);
+                }
+                this.walls = walls;
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .ant-descriptions {
+        margin-top: 20px;
+    }
 </style>
---
+++ src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/CardboardUsedEditor/CorrugatedCardboardProperties/WallDetails.vue
@@ -0,0 +1,21 @@
+<template>
+    <a-descriptions bordered>
+        <a-descriptions-item label="Product" :span="3">
+            Cloud Database
+        </a-descriptions-item>
+        <a-descriptions-item label="Product">
+            Cloud Database
+        </a-descriptions-item>
+    </a-descriptions>
+</template>
+
+<script>
+    export default {
+        name: "WallDetails",
+        props: ['wall', 'cardboard']
+    }
+</script>
+
+<style scoped>
+
+</style>

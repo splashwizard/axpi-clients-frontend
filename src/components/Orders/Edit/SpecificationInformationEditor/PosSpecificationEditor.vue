782088a133d45c08df7797ab2ec1b05aa830512f
Harvey Coplestone <harvey@enhanceable.io>
2021-02-14 22:37:37 +0000

    pos

--- src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor.vue
+++ src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor.vue
@@ -8,14 +8,24 @@
             <cardboard-used-editor :order-local="orderLocal"></cardboard-used-editor>
         </div>
         <!-- / Cardboard Used -->
+
+        <!-- Product Details -->
+        <div class="form-section">
+            <div class="form-header">
+                <h2>Product Details</h2>
+            </div>
+            <product-details :order-local="orderLocal"></product-details>
+        </div>
+        <!-- / Product Details -->
     </div>
 </template>
 
 <script>
     import CardboardUsedEditor from "./PosSpecificationEditor/CardboardUsedEditor";
+    import ProductDetails from "./PosSpecificationEditor/ProductDetails";
     export default {
         name: "PosSpecificationEditor",
-        components: {CardboardUsedEditor},
+        components: {CardboardUsedEditor, ProductDetails},
         props: ['orderLocal']
     }
 </script>
---
+++ src/components/Orders/Edit/SpecificationInformationEditor/PosSpecificationEditor/ProductDetails.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <a-form layout="vertical">
+            <a-form-item label="Width">
+                <a-input-group compact>
+                    <a-input size="large" style="width: 50%" v-model="orderLocal.pos_product_width"  />
+                    <a-select default-value="GBP" size="large" v-model="orderLocal.pos_product_width_unit">
+                        <a-select-option value="cm">
+                            cm
+                        </a-select-option>
+                        <a-select-option value="inch">
+                            inch
+                        </a-select-option>
+                    </a-select>
+                </a-input-group>
+            </a-form-item>
+
+            <a-form-item label="Depth">
+                <a-input-group compact>
+                    <a-input size="large" style="width: 50%" v-model="orderLocal.pos_product_depth"  />
+                    <a-select default-value="GBP" size="large" v-model="orderLocal.pos_product_depth_unit">
+                        <a-select-option value="cm">
+                            cm
+                        </a-select-option>
+                        <a-select-option value="inch">
+                            inch
+                        </a-select-option>
+                    </a-select>
+                </a-input-group>
+            </a-form-item>
+
+            <a-form-item label="Height">
+                <a-input-group compact>
+                    <a-input size="large" style="width: 50%" v-model="orderLocal.pos_product_height"  />
+                    <a-select default-value="GBP" size="large" v-model="orderLocal.pos_product_height_unit">
+                        <a-select-option value="cm">
+                            cm
+                        </a-select-option>
+                        <a-select-option value="inch">
+                            inch
+                        </a-select-option>
+                    </a-select>
+                </a-input-group>
+            </a-form-item>
+        </a-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProductDetails",
+        props: ['orderLocal']
+    }
+</script>
+
+<style scoped>
+
+</style>

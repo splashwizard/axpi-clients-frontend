681d6d5a43251a0f93abf21441c22641262be5a7
Harvey Coplestone <harvey@enhanceable.io>
2021-03-08 14:06:22 +0000

    apparel mass

--- src/components/Orders/Edit/SpecificationInformationEditor/ApparelSpecificationEditor.vue
+++ src/components/Orders/Edit/SpecificationInformationEditor/ApparelSpecificationEditor.vue
@@ -14,8 +14,39 @@
 
             <sweater-details v-if="orderLocal.product_subtype === 'sweater'"
                              :order-local="orderLocal"></sweater-details>
+
+            <!-- Item Mass -->
+            <a-form layout="vertical">
+                <a-row :gutter="70">
+                    <a-col span="12">
+                        <a-form-item label="Mass">
+                            <a-input-group compact>
+                                <a-input size="large" style="width: 70%" v-model="orderLocal.apparel_mass"/>
+                                <a-select default-value="GBP" style="width: 30%" size="large" v-model="orderLocal.apparel_mass_unit">
+                                    <a-select-option value="g">
+                                        g
+                                    </a-select-option>
+                                    <a-select-option value="lbs">
+                                        lbs
+                                    </a-select-option>
+                                </a-select>
+                            </a-input-group>
+                        </a-form-item>
+                    </a-col>
+                </a-row>
+            </a-form>
+            <!-- / Item Mass -->
         </div>
         <!-- / Basic Details -->
+
+        <!-- Materials -->
+        <div class="form-section">
+            <div class="form-header">
+                <h2>Materials</h2>
+            </div>
+            <material-used-editor :order-local="orderLocal"></material-used-editor>
+        </div>
+        <!-- / Materials -->
     </div>
 </template>
 
@@ -23,10 +54,11 @@
     import HoodieDetails from "./ApparelSpecificationEditor/HoodieDetails";
     import SweaterDetails from "./ApparelSpecificationEditor/SweaterDetails";
     import TShirtDetails from "./ApparelSpecificationEditor/TShirtDetails";
+    import MaterialUsedEditor from "./ApparelSpecificationEditor/MaterialUsedEditor";
 
     export default {
         name: "ApparelSpecificationEditor",
-        components: {HoodieDetails, SweaterDetails, TShirtDetails},
+        components: {HoodieDetails, SweaterDetails, TShirtDetails, MaterialUsedEditor},
         props: ['orderLocal']
     }
 </script>
---
+++ src/components/Orders/Edit/SpecificationInformationEditor/ApparelSpecificationEditor/MaterialUsedEditor.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+        <el-collapse v-model="activePanel" accordion>
+            <el-collapse-item v-for="(material, i) in orderLocal.apparel_material_used" :key="i"
+                              :title="getSectionNameLabel(material.material_type)">
+                <div class="collapse-inner-section">
+                    <a-row :row="70">
+                        <a-col :span="12">
+                            <a-form layout="vertical">
+                                <a-form-item label="Material">
+                                    <a-cascader size="large" v-model="material.material_type"
+                                            :options="materialTypeOptions" @change="forceRefresh">
+                                    </a-cascader>
+                                </a-form-item>
+
+                                <a-checkbox @change="forceRefresh">
+                                    Organic?
+                                </a-checkbox>
+
+                                <a-form-item label="Percentage">
+                                    <a-input v-model="material.percentage"
+                                             @blur="forceRefresh"
+                                            type="number" :min="0" :max="100" size="large"></a-input>
+                                </a-form-item>
+
+                                <a-form-item label="Country Of Origin">
+                                    <a-select show-search
+                                            v-model="material.country_of_origin" @change="forceRefresh" size="large">
+                                        <a-select-option v-for="(country, i) in countries" :value="country" :key="i">
+                                            {{ country }}
+                                        </a-select-option>
+                                    </a-select>
+                                </a-form-item>
+                            </a-form>
+                        </a-col>
+                    </a-row>
+                </div>
+            </el-collapse-item>
+        </el-collapse>
+
+        <!-- Add new material button -->
+        <a-button icon="plus" @click="addMaterial">Add material</a-button>
+        <!-- / Add new material button -->
+    </div>
+</template>
+
+<script>
+    import country from 'country-list-js';
+
+    const MATERIAL_USED_DATA_TEMPLATE = {
+        material_type: null,
+        percentage: null
+    };
+
+    const MATERIAL_TYPE_OPTIONS = [
+        {
+            label: 'Cotton',
+            value: 'Cotton',
+            children: [
+                {
+                    label: 'Combed Cotton',
+                    value: 'Combed Cotton'
+                },
+                {
+                    label: 'Pima Cotton',
+                    value: 'Pima Cotton'
+                },
+                {
+                    label: 'Slub Cotton',
+                    value: 'Slub Cotton'
+                }
+            ]
+        },
+        {
+            label: 'Linen',
+            value: 'Linen'
+        },
+        {
+            label: 'Polyester',
+            value: 'Polyester'
+        },
+        {
+            label: 'Rayon',
+            value: 'Rayon'
+        },
+        {
+            label: 'Silk',
+            value: 'Silk'
+        },
+        {
+            label: 'Lycra',
+            value: 'Lycra'
+        },
+        {
+            label: 'Bamboo',
+            value: 'Bamboo'
+        }
+    ];
+
+    export default {
+        name: "MaterialUsedEditor",
+        props: ['orderLocal'],
+        data() {
+            return {
+                activePanel: undefined,
+                materialTypeOptions: MATERIAL_TYPE_OPTIONS,
+                countries: country.names()
+            }
+        },
+        mounted() {
+            if (!this.orderLocal.apparel_material_used) {
+                this.orderLocal.apparel_material_used = [
+                    {
+                        ...MATERIAL_USED_DATA_TEMPLATE
+                    }
+                ];
+                this.forceRefresh();
+            }
+        },
+        methods: {
+            addMaterial() {
+                this.orderLocal.apparel_material_used.push(
+                    {
+                        ...MATERIAL_USED_DATA_TEMPLATE
+                    }
+                );
+                this.forceRefresh();
+                this.activePanel = Number(this.orderLocal.apparel_material_used.length - 1);
+            },
+
+            forceRefresh() {
+                this.$nextTick(() => {
+                    this.$forceUpdate();
+                });
+            },
+
+            getSectionNameLabel(value) {
+                if (!value) {
+                    return 'Please select a material';
+                }
+                return value.join(' / ');
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .el-collapse {
+        margin-bottom: 20px;
+    }
+</style>
